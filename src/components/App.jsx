574f72ff75490c430518e2a7705cd3c33a688078
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-08 00:15:10 -0500

    Configura prettier y eslint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import '../styles/App.css'
+import { useState } from 'react';
+import '../styles/App.css';
 
+const carajo = 0;
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
