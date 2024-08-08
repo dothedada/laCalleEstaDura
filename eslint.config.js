b4184ecb6c90f279d14d30985fae3f00d3ea35df
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-07 22:37:05 -0500

    Incorpora prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
   {
@@ -35,4 +36,5 @@ export default [
       ],
     },
   },
+  eslintPluginPrettierRecommended
 ]
