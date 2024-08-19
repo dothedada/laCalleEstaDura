5dcbf5a34f872654bf7d6cccf6703aa79edf9ecb
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-19 13:22:28 -0500

    Crea la clase contacto

--- src/js/card.js
+++ src/js/card.js
@@ -1,10 +1,51 @@
-const createId = (base) => {
+export const createId = () => {
+    const base = 16;
+    const randomRange = 1000000;
+
     const time = Number(new Date().getTime()).toString(base);
-    const random = Math.round(Math.random() * 1000000).toString(base);
+    const random = Math.round(Math.random() * randomRange)
+        .toString(base)
+        .padStart(4, '0');
+
     return `${time}_${random}`;
 };
-export default class {
-    constructor() {
-        this.id = createId(16);
+
+const checkTranslation = (spanish, english) => {
+    return !!(spanish && english);
+};
+
+export class Contact {
+    constructor({ reference, name, titleEsp, titleEng, email, phone }) {
+        this.id = createId();
+        this.reference = reference ?? `Referencia_${this.id.slice(-4)}`;
+        this.name = name;
+        this.titleEsp = titleEsp;
+        this.titleEng = titleEng;
+        this.translated = checkTranslation(this.titleEsp, this.titleEng);
+        this.email = email;
+        this.phone = phone;
     }
 }
+
+export class Profile extends Contact {
+    constructor({ location, link1, link2, ...contactInfo }) {
+        super(contactInfo);
+        this.location = location;
+        this.link1 = link1;
+        this.link2 = link2;
+    }
+}
+// contacto
+// (reference, name, titleEsp, titleEng, email, link1, link2, phone, location)
+// references
+// (reference, name, titleEsp, titleEng, email, phone)
+
+// skills
+// (reference, text*, list*)
+// perfil
+// (reference, text*)
+
+// experience
+// (reference, place, title*, time start, time end, description*)
+// education
+// (reference, place, title*, time start, time end)
--- src/js/test/card.test.js
+++ src/js/test/card.test.js
@@ -1,21 +1,45 @@
 import { describe, test, expect } from 'vitest';
-import Card from '../card';
+import { createId, Contact, Profile } from '../card';
 
 describe('Propiedades de la tarjeta', () => {
-    const tarjeta = new Card();
-
-    test('La clase Card crea un objeto', () => {
-        expect(typeof tarjeta).toBe('object');
-    });
-    test('El objeto tiene un id, aleatorio', () => {
-        expect(tarjeta.id).not.toBeUndefined();
-
+    test('createId genera 100 identidficadores aleatorios en un ciclo', () => {
         const ids = new Set();
-        for (let i = 0; i < 1000; i++) {
-            const card = new Card();
-            ids.add(card.id);
+
+        for (let i = 0; i < 100; i++) {
+            const randomID = createId();
+            ids.add(randomID);
         }
 
-        expect(ids.size).toBe(1000);
+        expect(ids.size).toBe(100);
+    });
+
+    const testContact = {
+        reference: 'Persona Contacto',
+        name: 'Jane Doe',
+        titleEsp: 'Persona',
+        email: 'info@mmejia.com',
+    };
+
+    test('La clase Contact crea un objeto', () => {
+        const contacto = new Contact(testContact);
+        expect(typeof contacto).toBe('object');
+    });
+
+    test('El objeto tiene id, referencia, cargo en español, cargo en inglés, correo y teléfono', () => {
+        const tarjeta = new Contact(testContact);
+        expect(tarjeta.id).not.toBeUndefined();
+        expect(tarjeta.reference).not.toBeUndefined();
+        expect(tarjeta.titleEsp).not.toBeUndefined();
+        expect(tarjeta.email).not.toBeUndefined();
+    });
+
+    test('Translated retorna falso si falta alguna versiond e title', () => {
+        const contacto = new Contact(testContact);
+        expect(contacto.translated).toBe(false);
+    });
+
+    test('Translated retorna true si falta alguna versiond e title', () => {
+        const contacto = new Contact({ ...testContact, titleEng: 'person' });
+        expect(contacto.translated).toBe(true);
     });
 });
