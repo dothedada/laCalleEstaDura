743b37fc9fe2c79adab4d67fe05c5ef2d87ddb0b
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-21 10:08:55 -0500

    Crea los tests para la clase Deck

--- src/js/test/deck.test.js
+++ src/js/test/deck.test.js
@@ -52,18 +52,21 @@ describe('Propiedades de Deck', () => {
             expect(randomSpy).toBeCalled();
             expect(regex.test(testDeck.id)).toBe(true);
 
-            console.log(testDeck);
             vitest.restoreAllMocks;
         });
     });
 
     describe('Propiedades de la lista', () => {
         test('tiene una propiedad llamada cards', () => {
-            expect('cards' in deck).toBe(true);
+            expect('getCards' in deck).toBe(true);
         });
 
         test('list es un set para evitar tarjetas duplicadas', () => {
-            expect(deck.cards instanceof Set).toBe(true);
+            expect(deck.getCards instanceof Set).toBe(true);
+        });
+
+        test('Si a la creación del deck no se especifica ninguna carta, es un set vacío', () => {
+            expect(deck.getCards.size).toBe(0);
         });
 
         test('Se pueden cargar cartas al contruir el deck', () => {
@@ -72,7 +75,43 @@ describe('Propiedades de Deck', () => {
                 cards: ['card1', 'card2', 'card3'],
             });
 
-            expect(testDeck.cards.size).toBe(3)
+            expect(testDeck.getCards.size).toBe(3);
+        });
+
+        test('Permite añadir una o varias cartas por su id luego de creado el deck', () => {
+            deck.addCards('cardID');
+            expect(deck.getCards.size).toBe(1);
+            deck.addCards(['card1', 'card2', 'card3']);
+            expect(deck.getCards.size).toBe(4);
+        });
+
+        test('sólo se puede editar la lista de cartas a través de setters', () => {
+            deck.cards = 'someCardId';
+            expect(deck.getCards.size).toBe(0);
+            expect(() => {
+                deck.getCards = 'someCardId';
+            }).toThrowError();
+        });
+
+        test('Permite eliminar una o varias cartas por su id', () => {
+            deck.addCards(['card1', 'card2', 'card3']);
+            deck.removeCards('card1');
+            expect(deck.getCards.size).toBe(2);
+            deck.removeCards(['card2', 'card3']);
+            expect(deck.getCards.size).toBe(0);
+        });
+
+        test('Permite actualizar un deck frente a una lista de ids', () => {
+            deck.addCards(['card1', 'card2', 'card3']);
+            deck.updateCards(['card2', 'cardA', 'cardB']);
+
+            expect(deck.getCards.has('card1')).toBe(false)
+            expect(deck.getCards.has('cardA')).toBe(true)
+            expect(deck.getCards.has('card2')).toBe(true)
+
+            deck.updateCards('cardX')
+            expect(deck.getCards.has('cardA')).toBe(false)
+            expect(deck.getCards.has('cardX')).toBe(true)
         });
     });
 });
