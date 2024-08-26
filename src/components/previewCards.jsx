3076ddff2344c2abf3b3c1de1d978185e6dcdfbc
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-26 16:40:45 -0500

    Crea las funciones para dar formato a los Objetos Dateen las previsualizaciones

--- src/components/previewCards.jsx
+++ src/components/previewCards.jsx
@@ -1,12 +1,26 @@
+import { months } from './txtAndValidations';
+
+const formatDate = (date) => `${months[date.getMonth()]} ${date.getFullYear()}`;
+
+const dateLabel = (date) => {
+    const formatedMonth = `${+date.getMonth() + 1}`.padStart(2, '0');
+    return `${date.getFullYear()}-${formatedMonth}`;
+};
+
 const ExperiencePreview = ({ data, lang }) => {
+    const timeStart = formatDate(data.timeStart);
+    const timeEnd = formatDate(data.timeEnd);
+    const timeLabelStart = dateLabel(data.timeStart);
+    const timeLabelEnd = dateLabel(data.timeEnd);
+
     return (
         <article>
             <header>
                 <h3>
                     {data.place}
                     <span className="date">
-                        <time dateTime="2012-03">{data.timeStart}</time>-
-                        <time dateTime="2014-07">{data.timeEnd}</time>
+                        <time dateTime={timeLabelStart}>{timeStart} </time>-
+                        <time dateTime={timeLabelEnd}> {timeEnd}</time>
                     </span>
                 </h3>
                 <div className="title">{data[`title${lang}`]}</div>
