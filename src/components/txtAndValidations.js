8c9eb747bbc461348fae2c56ff682c7bc4228dd0
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-25 21:15:29 -0500

    Agrega regex para meses y años

--- src/components/txtAndValidations.js
+++ src/components/txtAndValidations.js
@@ -18,6 +18,26 @@ export const inputValidation = {
     }),
 };
 
+export const findInString = {
+    month: /\b(1[0-2]|0?[1-9]|enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)\b/,
+    year: /\b(\d{4})\b/,
+};
+
+export const months = [
+    'enero',
+    'febrero',
+    'marzo',
+    'abril',
+    'mayo',
+    'junio',
+    'julio',
+    'agosto',
+    'septiembre',
+    'octubre',
+    'noviembre',
+    'diciembre',
+];
+
 export const iconsPaths = {
     edit: {
         true: 'M208 80H96V56a32 32 0 0 1 32-32c15.37 0 29.2 11 32.16 25.59a8 8 0 0 0 15.68-3.18C171.32 24.15 151.2 8 128 8a48.05 48.05 0 0 0-48 48v24H48a16 16 0 0 0-16 16v112a16 16 0 0 0 16 16h160a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16M48 128h160v16H48Zm0 32h160v16H48Zm160-64v16H48V96Zm0 112H48v-16h160z',
