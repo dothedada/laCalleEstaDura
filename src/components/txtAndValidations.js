c8c1616aebd7145b4113d80e58aaf13b3a027ff1
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-08-29 16:29:20 -0500

    Extrae la función para parseo de fechas del objeto Education

--- src/components/txtAndValidations.js
+++ src/components/txtAndValidations.js
@@ -38,6 +38,27 @@ export const months = [
     'diciembre',
 ];
 
+const parseMonth = (month) => {
+    return /\d/.test(month)
+        ? +month - 1
+        : months.findIndex((m) => m === month.toLowerCase());
+};
+
+export const parseDate = (date) => {
+    if (!date) return new Date();
+
+    const cleanDate = date instanceof Date ? date.toDateString() : date;
+    const year = cleanDate.match(findInString.year)[0];
+    const month = parseMonth(cleanDate.match(findInString.month)[0]);
+
+    const newDate = new Date();
+    newDate.setDate(1);
+    newDate.setMonth(month);
+    newDate.setFullYear(year);
+
+    return newDate;
+};
+
 export const iconsPaths = {
     edit: {
         true: 'M208 80H96V56a32 32 0 0 1 32-32c15.37 0 29.2 11 32.16 25.59a8 8 0 0 0 15.68-3.18C171.32 24.15 151.2 8 128 8a48.05 48.05 0 0 0-48 48v24H48a16 16 0 0 0-16 16v112a16 16 0 0 0 16 16h160a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16M48 128h160v16H48Zm0 32h160v16H48Zm160-64v16H48V96Zm0 112H48v-16h160z',
