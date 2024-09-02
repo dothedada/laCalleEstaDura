33cd7244fc6a1d44c2512ed4c9d10b05c3fe5600
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-09-02 17:11:09 -0500

    Crea el componente bioForm.jsx

---
+++ src/components/bioForm.jsx
@@ -0,0 +1,121 @@
+import { useRef, useState } from 'react';
+
+import { Input, FormButtons, Container, Bar, Fieldset } from './formComponents';
+import { ExperiencePreview } from './previewCards.jsx';
+import {
+    inputValidation,
+    formValidation,
+    uiText,
+} from './txtAndValidations.js';
+import {
+    propGenerator,
+    resetData,
+    deleteData,
+    saveData,
+    getFieldValidation,
+} from './formMethods.js';
+
+const ExperienceForm = ({ data, inPdf = true, inPdfCallback }) => {
+    // se va para arriba luego
+    const [renderInPdf, setRenderInPdf] = useState(inPdf);
+    const inPdfHandler = () => {
+        console.log(inPdfCallback);
+        setRenderInPdf(!renderInPdf);
+    };
+
+    // Card states
+    const [openToEdit, setOpenToEdit] = useState(false);
+    const [startingData] = useState(data || undefined);
+    const [dataToInject, setDataToInject] = useState(() =>
+        startingData ? structuredClone(startingData) : {},
+    );
+    const [globalValidations, setGlobalValidations] = useState([]);
+
+    // form inputs
+    const refs = {
+        descriptionEsp: useRef(),
+        descriptionEng: useRef(),
+    };
+    const props = propGenerator(
+        'bio',
+        refs,
+        dataToInject,
+        setDataToInject,
+    );
+
+    // card handlers
+    const handleDelete = () => deleteData(startingData);
+    const handleReset = () => resetData(startingData, setDataToInject);
+    const handleSave = () => {
+        saveData(
+            'bio',
+            refs,
+            startingData,
+            dataToInject,
+            //form validations array
+            [],
+            // setters
+            {
+                setGlobalValidations,
+                setDataToInject,
+                setRenderInPdf,
+                setOpenToEdit,
+            },
+        );
+    };
+
+    return (
+        <div className="card__config" id={'cardID'}>
+            <Bar
+                type="experience"
+                data={dataToInject}
+                open={openToEdit}
+                editHandler={() => setOpenToEdit(!openToEdit)}
+                inPdf={renderInPdf}
+                inPdfHandler={inPdfHandler}
+            />
+
+            <Container
+                open={openToEdit}
+                preview={
+                    renderInPdf && <ExperiencePreview data={startingData} />
+                }
+            >
+                <Input {...props('reference')} ref={null} />
+                <hr />
+
+                <Fieldset legend={uiText.experience.legend.description}>
+                    <Input
+                        {...props('descriptionEsp')}
+                        height="5"
+                        oneLine={false}
+                        validations={[
+                            inputValidation.notEmpty,
+                            inputValidation.maxLength(350),
+                        ]}
+                    />
+
+                    <Input
+                        {...props('descriptionEng')}
+                        oneLine={false}
+                        height="5"
+                        validations={[inputValidation.maxLength(350)]}
+                        sugestTranslation={
+                            dataToInject.descriptionEsp &&
+                            !dataToInject.descriptionEng
+                        }
+                    />
+                </Fieldset>
+
+                <FormButtons
+                    previousData={startingData}
+                    deleteCallback={handleDelete}
+                    resetCallback={handleReset}
+                    saveCallback={handleSave}
+                />
+            </Container>
+        </div>
+    );
+};
+
+export default ExperienceForm;
