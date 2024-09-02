507840e7724de88ed3ff5fd8805e15458ccb3dfd
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-09-01 19:42:57 -0500

    crea el objeto para la carga inicialw de todas las tarjetas

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,18 +3,31 @@ import { createRoot } from 'react-dom/client';
 import '../styles/reset.css';
 import '../styles/styles.css';
 import ExperienceForm from './experienceForm';
-import { Experience } from '../js/card';
+import cardClass from '../js/card';
 
-const storedCards = Object.keys(localStorage).map((cardId) =>
-    JSON.parse(localStorage.getItem(cardId)),
-);
-const experienceCards = storedCards
-    .filter((card) => card.type === 'experience')
-    .map((card) => new Experience(card));
+// TODO:
+// 6. implementación en otros tipos de tarjetas
+// 6.a. creación componentes vista previa
+// 7. creación del componente contenedor de los dormularios
+// 7.a. levantar el estado del render en pdf
+// 7.b creacion del modelo base
+// 8. creación del pdf
+
+const storedCards = Object.keys(localStorage)
+    .map((cardId) => JSON.parse(localStorage.getItem(cardId)))
+    .reduce((deck, card) => {
+        if (!deck[card.type]) {
+            deck[card.type] = [];
+        }
+        deck[card.type].push(new cardClass[card.type](card));
+        return deck;
+    }, {});
+
+console.log(storedCards)
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        {experienceCards.map((card) => (
+        {storedCards.experience.map((card) => (
             <ExperienceForm data={card} key={card.id} />
         ))}
         <ExperienceForm />
