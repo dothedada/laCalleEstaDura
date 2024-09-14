8fd47b9c534c9869c76601578da9ee8b4b559c57
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-09-14 13:00:11 -0500

    Separa previsualizacion de formulario y crea la previsualizacion en limpio

---
+++ src/components/profilePreview.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+
+import { Bar } from './formComponents';
+
+const ProfilePreview = ({ data, lang= 'Eng', inPdf = true, inPdfCallback }) => {
+    // se va para arriba luego
+    const [renderInPdf, setRenderInPdf] = useState(inPdf);
+    const inPdfHandler = () => {
+        setRenderInPdf(!renderInPdf);
+    };
+
+    const [startingData] = useState(data || undefined);
+
+    return (
+        <div className="card" id={'cardID'}>
+            <Bar
+                data={startingData}
+                editHandler={() => console.log('edita')}
+                duplicateHandler={() => console.log('duplica')}
+                inPdf={renderInPdf}
+                inPdfHandler={inPdfHandler}
+            />
+
+            {renderInPdf && (
+                <div className="card__preview twoCol">
+                    <div>
+                        <h2 className="name">{data.name}</h2>
+                        <div className="title">{data[`title${lang}`]}</div>
+                    </div>
+                    <div>
+                        <div>
+                            <a href={`mailto:${data.email}`}>{data.email}</a>
+                        </div>
+                        {data.link1 && (
+                            <div>
+                                <a href={data.link1} target="_blank">
+                                    {data.link1}
+                                </a>
+                            </div>
+                        )}
+                        {data.link2 && (
+                            <div>
+                                <a href={data.link2} target="_blank">
+                                    {data.link2}
+                                </a>
+                            </div>
+                        )}
+                        <div>{data.phone}</div>
+                        <div>{data.location}</div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ProfilePreview;
