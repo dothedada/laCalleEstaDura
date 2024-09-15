1b299db316c1838858ce0e650fd81aa6a8b51876
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-09-15 07:42:15 -0500

    Eleva el estado y el SetState de renderizado en pdf

--- src/components/decksManager.jsx
+++ src/components/decksManager.jsx
@@ -16,56 +16,28 @@ const cardGroups = [
 const DeckManager = ({ cards }) => {
     const [storedCards, setStoredCards] = useState(cards);
 
+    const [renderInPdf, setRenderInPdf] = useState(new Set());
+
+    const inPdfHandler = (id) => () => {
+        setRenderInPdf((prvRender) => {
+            const newRender = new Set(prvRender);
+
+            if (renderInPdf.has(id)) {
+                newRender.delete(id);
+            } else {
+                newRender.add(id);
+            }
+
+            return newRender;
+        });
+    };
+
     return (
-        <>
-            <h1>la calle esta dura</h1>
-            <div className="frame decks">
-                <div className="cv-selector">
-                    <select name="cvs" id="cvs_selector">
-                        <option>carajo</option>
-                    </select>
-
-                    <div className="cv-actions">
-                        <button type="button">Añadir</button>
-                        <button type="button">Actualizar</button>
-                        <button type="button">Eliminar</button>
-                    </div>
-                </div>
-
-                {cardGroups.map((deckType, index) => (
-                    <div key={index}>
-                        <h2>{uiText.global.sections.Esp[deckType]}</h2>
-
-                        {storedCards?.[deckType]?.map((card) => (
-                            <DynamicCard
-                                type={card.type}
-                                key={card.id}
-                                data={card}
-                                lang="Esp"
-                                inPdf={false}
-                                inPdfCallback={() => console.log('carajillo')}
-                            />
-                        ))}
-
-                        {deckType !== 'skills' ? (
-                            <Button
-                                type="button"
-                                text={uiText[deckType].reference}
-                            />
-                        ) : (
-                            <>
-                                <Button
-                                    type="button"
-                                    text={uiText.skillsList.reference}
-                                />
-                                <Button
-                                    type="button"
-                                    text={uiText.skillsText.reference}
-                                />
-                            </>
-                        )}
-                    </div>
-                ))}
+        <div className="frame decks">
+            <div className="cv-selector">
+                <select name="cvs" id="cvs_selector">
+                    <option>carajo</option>
+                </select>
 
                 <div className="cv-actions">
                     <button type="button">Añadir</button>
@@ -73,7 +45,48 @@ const DeckManager = ({ cards }) => {
                     <button type="button">Eliminar</button>
                 </div>
             </div>
-        </>
+
+            {cardGroups.map((deckType, index) => (
+                <div key={index}>
+                    <h2>{uiText.global.sections.Esp[deckType]}</h2>
+
+                    {storedCards?.[deckType]?.map((card) => (
+                        <DynamicCard
+                            type={card.type}
+                            key={card.id}
+                            data={card}
+                            lang="Esp"
+                            inPdf={renderInPdf.has(card.id)}
+                            inPdfCallback={inPdfHandler(card.id)}
+                        />
+                    ))}
+
+                    {deckType !== 'skills' ? (
+                        <Button
+                            type="button"
+                            text={uiText[deckType].reference}
+                        />
+                    ) : (
+                        <>
+                            <Button
+                                type="button"
+                                text={uiText.skillsList.reference}
+                            />
+                            <Button
+                                type="button"
+                                text={uiText.skillsText.reference}
+                            />
+                        </>
+                    )}
+                </div>
+            ))}
+
+            <div className="cv-actions">
+                <button type="button">Añadir</button>
+                <button type="button">Actualizar</button>
+                <button type="button">Eliminar</button>
+            </div>
+        </div>
     );
 };
 
