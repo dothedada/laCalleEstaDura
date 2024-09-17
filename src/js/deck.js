b217954a4227f892f45d954d7420df86dda0038b
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-09-16 19:24:58 -0500

    Inicia el desarrollo de la clase deck actualizada

--- src/js/deck.js
+++ src/js/deck.js
@@ -1,6 +1,74 @@
 // TODO:
 // Posibilidad de personalizar el orden de algunas secciones
 // (Habilidades, referencias, Formacion)
+import cardClass from './card';
+
+export class MainDeck {
+    constructor() {
+        const inLS = this.loadLS();
+
+        this.subDecks = inLS.cards.reduce(this.addCardToDeck);
+        this.sets = inLS.sets;
+    }
+
+    // sets = [{setId: keygen, cardsIds: []}]
+
+    loadLS() {
+        return Object.keys(localStorage).reduce(
+            (items, key) => {
+                const item = JSON.parse(localStorage.getItem(key));
+                // if (!/LCED/g.test(item.id)) return items;
+
+                const { type } = item;
+                (type ? items.cards : items.sets).push(item);
+
+                return items;
+            },
+            { cards: [], sets: [] },
+        );
+    }
+
+    getDeckType(type) {
+        return /^skills/.test(type) ? 'skills' : type;
+    }
+
+    addCardToDeck(deck, card) {
+        if (!card.type) {
+            return deck;
+        }
+        const deckType = this.getDeckType(card.type);
+        deck[deckType] = deck[deckType] || [];
+        deck[deckType].push(new cardClass[card.type](card));
+
+        return deck;
+    }
+
+    removeFromSets({ id, type }) {
+        this.sets.forEach((set) => {
+            const cardIdIndex = set.cardsIds.indexOf((cardId) => cardId === id);
+
+            if (cardIdIndex > 0) {
+                set.cardsIds.splice(cardIdIndex, 1);
+            }
+        });
+
+        const deckType = this.getDeckType(type);
+        const indexOfCard = this.subDecks[deckType].indexOf(
+            (card) => card.id === id,
+        );
+        this.subDecks[deckType].splice(indexOfCard, 1);
+    }
+
+    updateCardOnDeck({ id, type }) {
+        const deckType = this.getDeckType(type);
+        const indexOfCard = this.subDecks[deckType].indexOf(
+            (card) => card.id === id,
+        );
+        const updatedCard = new cardClass[deckType](localStorage.getItem(id));
+
+        this.subDecks[deckType].splice(indexOfCard, 1, updatedCard);
+    }
+}
 
 export class Deck {
     #cards = new Set();
