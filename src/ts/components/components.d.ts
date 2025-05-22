e543d6044370c572e3d5fdc0c97a9f8fd81a2253
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-05-22 11:14:53 -0500

    feat(form): Adds basic input elements and types

---
+++ src/ts/components/components.d.ts
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Tags } from '../types';
+
+export type InputFieldProps = {
+    type: 'text' | 'url' | 'email' | string;
+    placeholder?: string;
+    name: string;
+    baseValue?: string;
+};
+
+export type TextareaProps = Omit<InputFieldProps, 'type'>;
+
+export type SubmitProps = 'new' | 'update' | 'derivate' | 'delete';
+export type FormProps = {
+    children: React.ReactNode;
+    id: Tags;
+};
