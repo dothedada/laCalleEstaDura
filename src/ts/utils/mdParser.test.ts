effb18dfb094c1d0ca6e49465a7ba5e33207c60f
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-05-26 21:27:45 -0500

    feat(parser): creates paring methods

--- src/ts/utils/mdParser.test.ts
+++ src/ts/utils/mdParser.test.ts
@@ -1,10 +1,22 @@
 import { describe, it, expect } from 'vitest';
-import { parseMD } from './mdParser';
+import { charComparison, parseInputMD } from './mdParser';
+
+describe('charComparison', () => {
+    it('should return false if any of the params is Undefined', () => {
+        expect(charComparison(undefined, undefined)).toBe(false);
+        expect(charComparison('a', undefined)).toBe(false);
+        expect(charComparison(undefined, 'a')).toBe(false);
+    });
+
+    it('should return true if patternChar is "char" and any char is passed', () => {
+        expect(charComparison('a', 'char'));
+    });
+});
 
 describe('Markdown parser for user input', () => {
     it('Should take an string and return a Node', () => {
-        const input = 'This is some shitty text';
-        const parsedMD = parseMD(input);
+        const input = '**hola _holi_ chao**';
+        const parsedMD = parseInputMD(input);
 
         expect(parsedMD).toEqual(
             expect.objectContaining({
@@ -14,17 +26,17 @@ describe('Markdown parser for user input', () => {
         );
     });
 
-    it('should contain a Node with label "strong" if a strong characters are passed', () => {
-        const inputA = '**i am strong** ';
-        const inputB = 'This is **strong**';
-        const inputC = '__also this__ is strong';
-        const inputD = 'and __this__ one';
-
-        const parseA = parseMD(inputA);
-        const parseB = parseMD(inputB);
-        // const parseC = parseMD(inputC);
-        // const parseD = parseMD(inputD);
-
-        expect(Array.isArray(parseA.content)).toBe(true);
-    });
+    // it('should contain a Node with label "strong" if a strong characters are passed', () => {
+    //     const inputA = '**i am strong** ';
+    //     const inputB = 'This is **strong**';
+    //     const inputC = '__also this__ is strong';
+    //     const inputD = 'and __this__ one';
+    //
+    //     const parseA = parseMD(inputA);
+    //     const parseB = parseMD(inputB);
+    //     // const parseC = parseMD(inputC);
+    //     // const parseD = parseMD(inputD);
+    //
+    //     expect(Array.isArray(parseA.content)).toBe(true);
+    // });
 });
--- src/ts/utils/mdParser.ts
+++ src/ts/utils/mdParser.ts
@@ -1,74 +1,185 @@
-export interface Node<
-    T extends keyof HTMLElementTagNameMap = keyof HTMLElementTagNameMap,
-> {
-    label: T;
-    content: (Node | string)[] | string;
-    attributes?: Partial<HTMLElementTagNameMap[T]>;
-}
+type NodeLabels = 'strong' | 'em' | 'mark' | 'text';
+export type Node = {
+    label: NodeLabels;
+    position?: number;
+    content: Node[] | string;
+};
 
-function charComparison(
+type Pattern = ('end' | 'char' | 'digit' | string)[];
+type InOutPattern = { open: Pattern; close: Pattern };
+export type Segments = {
+    labelled: [number, number][];
+    plain: [number, number][];
+    pad: { start: number; end: number };
+};
+
+const BOUNDARY_CHARS = new Set([' ', '\t', '\n', '\0']);
+const PARSING_SEQUENCE = ['strong', 'em', 'mark'];
+
+export const patternsFor: { [k in Exclude<NodeLabels, 'text'>]: InOutPattern } =
+    {
+        strong: {
+            open: ['end', '*', '*', 'char'],
+            close: ['char', '*', '*', 'end'],
+        },
+        em: {
+            open: ['end', '_', 'char'],
+            close: ['char', '_', 'end'],
+        },
+        mark: {
+            open: ['end', '=', '=', 'char'],
+            close: ['char', '=', '=', 'end'],
+        },
+    };
+
+export function charComparison(
     char: string | undefined,
     patternChar: string | undefined,
 ): boolean {
     if (!char || !patternChar) {
         return false;
     }
-    if (patternChar === 'char') {
-        return char.trim() !== '';
+    switch (patternChar) {
+        case 'end':
+            return BOUNDARY_CHARS.has(char);
+        case 'char':
+            return !BOUNDARY_CHARS.has(char);
+        case 'digit':
+            return !isNaN(parseInt(char));
+        default:
+            return char === patternChar;
     }
-    return char === patternChar;
 }
 
-function findPattern(pattern: string[]) {
+function createPatternMatcher(
+    pattern: Pattern,
+): (c: string | undefined) => boolean {
     let patternIndex = 0;
 
-    return function (character: string | undefined): boolean {
-        if (!character) {
-            return false;
+    return function (character) {
+        if (!character || !charComparison(character, pattern[patternIndex])) {
+            patternIndex = 0;
         }
 
         if (charComparison(character, pattern[patternIndex])) {
             patternIndex++;
+        }
 
-            if (patternIndex === pattern.length) {
-                patternIndex = 0;
-                return true;
-            }
+        if (patternIndex === pattern.length) {
+            patternIndex = 0;
+            return true;
         }
 
         return false;
     };
 }
 
-export function findSection(
+function findPatternMatches(
     input: string,
-    searchPattern: string[],
+    searchPattern: InOutPattern,
 ): [number, number][] {
-    if (input === '') {
-        return [];
-    }
-    const openPattern = findPattern(searchPattern);
-    const closePattern = findPattern([...searchPattern].reverse());
-    const closeOffset = searchPattern.length - 1;
-    const openSliceIndex: number[] = [];
-    const workString = ` ${input} `;
-    const slices: [number, number][] = [];
+    const boundedInput = `\0${input}\0`;
+    const foundOpenPattern = createPatternMatcher(searchPattern.open);
+    const foundClosePattern = createPatternMatcher(searchPattern.close);
+    const openedStack: number[] = [];
+    const matches: [number, number][] = [];
+    const patternOpenOffset = searchPattern.open.length - 1;
+    const patternCloseOffset = 1;
 
-    for (let i = 0; i < workString.length; i++) {
-        if (openPattern(workString[i])) {
-            openSliceIndex.push(i - 1);
+    for (let i = 0; i < boundedInput.length; i++) {
+        if (foundOpenPattern(boundedInput[i])) {
+            openedStack.push(i - patternOpenOffset);
         }
-        if (openSliceIndex.length && closePattern(workString[i])) {
-            slices.push([openSliceIndex.pop()!, i - closeOffset]);
+        if (openedStack.length && foundClosePattern(boundedInput[i])) {
+            matches.push([openedStack.pop()!, i - patternCloseOffset]);
         }
     }
-    return slices;
+
+    return matches.filter(([start, end]) => start !== end);
 }
 
-export function parseMD(input: string): Node {
-    const node: Node<'p'> = {
-        label: 'p',
+function makePlainSegments(
+    markedSegments: [number, number][],
+    inputLength: number,
+): [number, number][] {
+    if (markedSegments.length === 0) {
+        return [[0, inputLength]];
+    }
+
+    let pos = 0;
+    const segments: [number, number][] = [];
+
+    for (const [start, end] of markedSegments) {
+        if (start < pos) {
+            pos = end;
+        }
+        segments.push([pos, start]);
+        pos = end;
+    }
+
+    if (pos < inputLength) {
+        segments.push([pos, inputLength]);
+    }
+
+    return segments.filter(([start, end]) => start !== end);
+}
+
+export function makeLabelNodes(
+    label: Exclude<NodeLabels, 'text'>,
+    input: string,
+): Node[] {
+    const labelMatches = findPatternMatches(input, patternsFor[label]);
+    const plainSegments = makePlainSegments(labelMatches, input.length);
+
+    const plainNodes = plainSegments.map(([start, end]) => {
+        return {
+            label: 'text',
+            position: start,
+            content: input.slice(start, end),
+        } as Required<Node>;
+    });
+    const labelNodes = labelMatches.map(([start, end]) => {
+        return {
+            label,
+            position: start,
+            content: input.slice(start, end),
+        } as Required<Node>;
+    });
+
+    const nodes: Node[] = [...plainNodes, ...labelNodes].sort(
+        (a, b) => a.position - b.position,
+    );
+
+    return nodes;
+}
+
+export function parseInputMD(input: string): Node {
+    const mainNode: Node = {
+        label: 'text',
         content: input,
     };
-    return node;
+
+    function parseContent(node: Node, label: Exclude<NodeLabels, 'text'>) {
+        const nodeContent = node.content;
+        if (Array.isArray(nodeContent)) {
+            nodeContent.forEach((node) => parseContent(node, label));
+        }
+
+        if (typeof nodeContent === 'string') {
+            const parsedNodes = makeLabelNodes(
+                label as Exclude<NodeLabels, 'text'>,
+                nodeContent,
+            );
+
+            node.content = parsedNodes;
+        }
+    }
+
+    for (const label of PARSING_SEQUENCE) {
+        parseContent(mainNode, label as Exclude<NodeLabels, 'text'>);
+    }
+
+    console.log(JSON.stringify(mainNode, null, 2));
+
+    return mainNode;
 }
